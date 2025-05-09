76b0e264e08e04f484ec0f73e659be9d9670c35d
Yulia Fil <filyulia.yf@gmail.com>
2025-05-09 21:53:01 +0300

    start moveDown method, add createNewCell(), clearCell() methods

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -21,19 +21,109 @@ class Game {
    * initial state.
    */
   constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+    this.initialState = initialState;
+    this.currentState = initialState.map((el) => [...el]);
+  }
+
+  createNewCell(moveDone = true) {
+    if (!moveDone) {
+      return;
+    }
+
+    let row, cell;
+
+    do {
+      row = Math.floor(Math.random() * 4);
+      cell = Math.floor(Math.random() * 4);
+    } while (this.currentState[row][cell] !== 0);
+
+    const tbody = document.querySelector('tbody');
+    const newCell = tbody.children[row].children[cell];
+
+    newCell.classList.add('field-cell--2');
+    newCell.textContent = '2';
+    this.currentState[row][cell] = 2;
+    newCell.style.color = 'red'; //delete after debug
+  }
+
+  clearCell(cell) {
+    cell.className = 'field-cell';
+    cell.textContent = '';
   }
 
   moveLeft() {}
   moveRight() {}
   moveUp() {}
-  moveDown() {}
+  moveDown() {
+    const tbody = document.querySelector('tbody');
+    let moveDone = false;
+
+    for (let row = 2; row >= 0; row--) {
+      for (let cell = 0; cell < 4; cell++) {
+        let currentCell = this.currentState[row][cell];
+
+        if (currentCell === 0) {
+          continue;
+        }
+
+        let lowerCell = this.currentState[row + 1][cell];
+        let n = 1;
+        let k = 0;
+
+        while (lowerCell === 0 && row + n < 4) {
+          moveDone = true;
+          lowerCell = currentCell;
+
+          const currentCellTd = tbody.children[row + k].children[cell];
+          this.clearCell(currentCellTd);
+
+          const lowerCellTd = tbody.children[row + n].children[cell];
+
+          lowerCellTd.textContent = lowerCell;
+          lowerCellTd.className = 'field-cell';
+          lowerCellTd.classList.add(`field-cell--${lowerCell}`);
+          lowerCellTd.style.color = ''; //delete after debug
+          this.currentState[row + n][cell] = lowerCell;
+          this.currentState[row + k][cell] = 0;
+
+          n++;
+          k++;
+
+          if (row + k < 3) {
+            currentCell = this.currentState[row + k][cell];
+          }
+
+          if (row + n < 4) {
+            lowerCell = this.currentState[row + n][cell];
+          }
+        }
+
+        // if (lowerCell === currentCell) {
+        //   moveDone = true;
+        //   lowerCell = currentCell * 2;
+        //   currentCell = tbody.children[row].children[cell];
+        //   this.clearCell(currentCell);
+
+        //   const lowerCellTd = tbody.children[row + 1].children[cell];
+
+        //   lowerCellTd.textContent = lowerCell;
+        //   lowerCellTd.className = 'field-cell';
+        //   lowerCellTd.classList.add(`field-cell--${lowerCell}`);
+        //   this.currentState[row + 1][cell] = lowerCell;
+        //   this.currentState[row][cell] = 0;
+        // }
+      }
+    }
+    this.createNewCell(moveDone);
+  }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() {
+    this.scoreField = document.querySelector('game-score');
+    this.score = 0;
+  }
 
   /**
    * @returns {number[][]}
@@ -55,12 +145,22 @@ class Game {
   /**
    * Starts the game.
    */
-  start() {}
+  start() {
+    this.createNewCell();
+    document.querySelector('.message-start').classList.add('hidden');
+  }
 
   /**
    * Resets the game.
    */
-  restart() {}
+  restart() {
+    this.currentState = this.initialState;
+
+    const cells = [...document.querySelectorAll('td')];
+
+    cells.forEach(this.clearCell);
+    this.createNewCell();
+  }
 
   // Add your own methods here
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,50 @@
 'use strict';
 
 // Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const initialState = [
+ [0, 0, 0, 0],
+ [0, 0, 0, 0],
+ [0, 0, 0, 0],
+ [0, 0, 0, 0]];
+const game = new Game(initialState);
 
-// Write your code here
+// #region buttonStart
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', buttonFunction);
+
+function buttonFunction() {
+  if (button.classList.contains('start')) {
+    game.start();
+    button.classList.remove('start');
+    button.classList.add('restart');
+    button.textContent = 'Restart';
+  } else {
+    game.restart();
+  }
+}
+
+// #endregion
+
+document.addEventListener('keydown', keyDown);
+
+function keyDown(e) {
+  const key = e.key;
+
+  switch (key) {
+    case 'ArrowLeft':
+      game.moveLeft();
+      break;
+    case 'ArrowRight':
+      game.moveRight();
+      break;
+    case 'ArrowDown':
+      game.moveDown();
+      break;
+    case 'ArrowUp':
+      game.moveUp();
+      break;
+  }
+}
